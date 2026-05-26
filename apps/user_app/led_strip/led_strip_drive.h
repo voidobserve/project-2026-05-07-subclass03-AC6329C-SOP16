5040d567fb3da792ca306036f05e40c1833495cb
voidobserve <99012263+voidobserve@users.noreply.github.com>
2026-05-26 09:32:23 +0800

    2026-05-25-周一，CRC-2A88-38738756
    - 移植工程`project-2026-05-06-AC6329C-SOP16`的程序，进行修改
    - 2.4G遥控器目前只识别按键按下的事件，双击和多击事件只会识别到第一次按下，后续需要再优化
    - 目前只识别`0x53 0x4A`这个格式头的2.4G遥控器
    - 根据客户给的24键2.4G遥控器的面贴，修改了对应的键位的功能
    - 将设备的蓝牙名称修改为`Auto Light`
    - 将设备的广播头修改为`5A44000101E90003`
    
    用旧的KEY生成的软件码：【`5BF2-E244123D`】
    用新的KEY生成的软件码：【`2A88-38738756`】

---
+++ apps/user_app/led_strip/led_strip_drive.h
@@ -0,0 +1,29 @@
+
+#ifndef led_strip_drive_h
+#define led_strip_drive_h
+
+#include "board_ac632n_demo_cfg.h"
+#include "asm/ledc.h"
+#include "asm/gpio.h"
+
+
+
+
+
+
+
+void led_state_init(void);
+void led_gpio_init(void);
+void led_pwm_init(void);
+
+void fc_rgbw_driver(u8 r, u8 g, u8 b, u8 w);
+
+
+#endif
+
+
+
+
+
+
+
